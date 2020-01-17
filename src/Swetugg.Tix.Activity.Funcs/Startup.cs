5c7bd8a39aa4231215dfcbab265e4060820dbcba
Anders Ljusberg <andlju@gmail.com>
2020-01-17 13:28:05 +0100

    Maybe throwaway

--- src/Swetugg.Tix.Activity.Funcs/Startup.cs
+++ src/Swetugg.Tix.Activity.Funcs/Startup.cs
@@ -1,8 +1,14 @@
-﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+﻿using System;
 using Microsoft.Extensions.Configuration;
+using System.Data.Common;
+using System.Data.SqlClient;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using NEventStore;
+using NEventStore.Persistence.Sql;
+using NEventStore.Persistence.Sql.SqlDialects;
+using NEventStore.Serialization.Json;
 using Swetugg.Tix.Activity.Commands;
 using Swetugg.Tix.Activity.Domain;
 using Swetugg.Tix.Activity.Funcs.Options;
@@ -18,9 +24,21 @@ namespace Swetugg.Tix.Activity.Funcs
             builder.Services.AddOptions<ActivityOptions>()
                 .Configure<IConfiguration>((settings, configuration) => { configuration.Bind(settings); });
 
-            var eventStore = Wireup.Init().UsingInMemoryPersistence();
             builder.Services.AddSingleton<IEventPublisher, ServiceBusPublisher>();
-            builder.Services.AddSingleton(sp => DomainHost.Build(eventStore, sp.GetService<IEventPublisher>(), sp.GetService<ILoggerFactory>(), null));
+            builder.Services.AddSingleton(sp =>
+            {
+                var connectionString = Environment.GetEnvironmentVariable("TixDbConnection");
+                var sqlClientFactoryInstance = SqlClientFactory.Instance;
+
+                var eventStore = Wireup.Init()
+                    .UsingSqlPersistence(sqlClientFactoryInstance, connectionString)
+                    .WithDialect(new MsSqlDialect())
+                    .InitializeStorageEngine()
+                    .UsingJsonSerialization();
+
+                return DomainHost.Build(eventStore, sp.GetService<IEventPublisher>(),
+                        sp.GetService<ILoggerFactory>(), null);
+            });
 
             builder.Services.AddScoped<ActivityCommandListenerFunc, ActivityCommandListenerFunc>();
         }
