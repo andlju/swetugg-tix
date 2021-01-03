b514c302b28d143355c56af0f4e3cd0fbf56f6ac
Anders Ljusberg <andlju@gmail.com>
2021-01-03 18:26:23 +0100

    Upgrade pulumi

--- Prepare-LocalEnvironment.ps1
+++ Prepare-LocalEnvironment.ps1
@@ -7,9 +7,9 @@ pulumi stack select local
 
 pulumi up -y
 
-pulumi stack output activityAppSettings > ..\src\Swetugg.Tix.Activity.Funcs\local.settings.json
-pulumi stack output orderAppSettings > ..\src\Swetugg.Tix.Order.Funcs\local.settings.json
-pulumi stack output processAppSettings > ..\src\Swetugg.Tix.Process.Funcs\local.settings.json
-pulumi stack output apiAppSettings > ..\src\Swetugg.Tix.Api\local.settings.json
+pulumi stack output activityAppSettings --show-secrets > ..\src\Swetugg.Tix.Activity.Funcs\local.settings.json
+pulumi stack output orderAppSettings --show-secrets > ..\src\Swetugg.Tix.Order.Funcs\local.settings.json
+pulumi stack output processAppSettings --show-secrets > ..\src\Swetugg.Tix.Process.Funcs\local.settings.json
+pulumi stack output apiAppSettings --show-secrets > ..\src\Swetugg.Tix.Api\local.settings.json
 
 Set-Location $oldLocation
